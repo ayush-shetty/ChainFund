08f92b501e04678b89c844151d632d05de85f831
SharathSivamalaisamy <sharath.cs20@bmsce.ac.in>
2023-04-10 19:30:39 +0530

    Revert "home and profile front end"
    This reverts commit 7ed70842d7339097d4c17c14d754318f39871587.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,31 +1,8 @@
-import React ,{useState,useEffect} from 'react'
-import { DisplayCampaigns } from '../components';
-import { useStateContext } from '../context'
+import React from "react";
 
 const Home = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [campaigns, setCampaigns] = useState([]);
-
-    const { address, contract, getCampaigns } = useStateContext();
-
-    const fetchCampaigns = async () => {
-        setIsLoading(true);
-        const data = await getCampaigns();
-        setCampaigns(data);
-        setIsLoading(false);
-      }
-    useEffect(() => {
-        if(contract) fetchCampaigns();
-      }, [address, contract]);
-     
-    
     return (
-        <DisplayCampaigns 
-          title="All Campaigns"
-          isLoading={isLoading}
-          campaigns={campaigns}
-      />
-          
+        <div> Home</div>
     )
 }
 
