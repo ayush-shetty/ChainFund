f4e7c80c646e492f5a52cd40ef34a37ca91558c7
SharathSivamalaisamy <sharath.cs20@bmsce.ac.in>
2023-04-13 23:43:46 +0530

    Update Sidebar
    Added names to the Sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -7,14 +7,17 @@ import { navlinks } from '../constants';
 import { useDisconnect } from "@thirdweb-dev/react";
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
-    <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
+  <div className={`w-[60px] h-[55px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
+    <div className="flex flex-col items-center">
       {!isActive ? (
-        <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
+        <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2 mb-1" />
       ) : (
         <img src={imgUrl} alt="fund_logo" className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`} />
       )}
+      <span className="text-xs mt-1 text-white">{name}</span>
     </div>
-  )
+  </div>
+);
   
   const Sidebar = () => {
     const navigate = useNavigate();
@@ -27,11 +30,8 @@ const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
     };
   
     return (
-      <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
-        <Link to="/">
-          <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
-        </Link>
-  
+      <div className="flex justify-between items-center flex-col sticky top-5 h-[55vh]">
+       
         <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
           <div className="flex flex-col justify-center items-center gap-3">
             {navlinks.map((link) => (
@@ -54,7 +54,7 @@ const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
             ))}
           </div>
   
-          <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
+         
         </div>
       </div>
     )
