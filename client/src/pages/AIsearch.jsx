9da98e0565fe59365b9e230848c0afb14d3b5c9a
ayush-shetty <124496931+ayush-shetty@users.noreply.github.com>
2023-04-15 13:10:22 +0530

    API

--- client/src/pages/AIsearch.jsx
+++ client/src/pages/AIsearch.jsx
@@ -36,7 +36,8 @@ const AIsearch = () => {
 
       setIsLoading(true);
   
-      const response = await fetch('http://127.0.0.1:5000/analyze', {
+     
+       const response = await fetch('https://127.0.0.1:8000/analyze/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -44,9 +45,11 @@ const AIsearch = () => {
         body: JSON.stringify({
           content: who,
           campaigns: campaigns,
-        }),
+        }), 
       });
       const data = await response.json();
+
+
   
       setSuggestedCampaigns(data);
       setIsLoading(false);
@@ -93,3 +96,50 @@ const AIsearch = () => {
 export default AIsearch 
 
 //set GOOGLE_APPLICATION_CREDENTIALS="C:\Project\ChainFund\decent-micron-383707-d0395c4b3446.json"
+
+/*def analyze(request: dict):
+    content = request['content']
+
+    # Instantiates a client
+    client = language_v1.LanguageServiceClient()
+
+    document = language_v1.Document(
+        content=content,
+        type_=language_v1.Document.Type.PLAIN_TEXT
+    )
+
+    # Analyze the user input for entities and categories
+    userInputEntities = client.analyze_entities(
+        document=document
+    ).entities
+
+    userInputCategories = client.classify_text(
+        document=document
+    ).categories
+
+    # Filter the campaigns based on matching entities and categories
+    relevant_campaigns = []
+    for campaign in request['campaigns']:
+        campaignEntities = client.analyze_entities(
+            document=language_v1.Document(
+                content=campaign['description'],
+                type_=language_v1.Document.Type.PLAIN_TEXT
+            )
+        ).entities
+        campaignCategories = client.classify_text(
+            document=language_v1.Document(
+                content=campaign['description'],
+                type_=language_v1.Document.Type.PLAIN_TEXT
+            )
+        ).categories
+
+        matching_entities = [entity for entity in campaignEntities if
+                             any(inputEntity.name == entity.name for inputEntity in userInputEntities)]
+        matching_categories = [category for category in campaignCategories if
+                               any(inputCategory.name == category.name for inputCategory in userInputCategories)]
+
+        if len(matching_entities) > 0 or len(matching_categories) > 0:
+            relevant_campaigns.append(campaign)
+
+    return relevant_campaigns
+ */
